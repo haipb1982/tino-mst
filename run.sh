f77c21c9628e3fceecf7dd53862b559885908d26
haipb <phan.ba.hai@rewardstyle.com>
2021-04-14 10:50:41 +0700

    update script run

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 docker build .
 
 # run and publish port
-docker run --rm -it --network=host e3d148b6acff ip addr show eth0
+docker run --rm -it --network=host 39199308ac0a ip addr show eth0
 
 # docker stop all containers before remove
 docker stop $(docker ps -a --format="{{.ID}}")
@@ -12,3 +12,5 @@ docker rm $(docker ps -a --format="{{.ID}}")
 
 # docker rm all images
 docker rmi $(docker images -a --format="{{.ID}}")
+
+docker system prune
